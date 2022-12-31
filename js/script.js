eaebe1a6154bb6e920d39d633442ae892f88e266
Mr. Web Developer <117938690+Yahyobek-Coder@users.noreply.github.com>
2022-12-31 11:04:13 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,58 @@
+let days = document.getElementById('days');
+let hours = document.getElementById('hours');
+let minutes = document.getElementById('minutes');
+let seconds = document.getElementById('seconds');
+
+let dd = document.getElementById('dd');
+let hh = document.getElementById('hh');
+let mm = document.getElementById('mm');
+let ss = document.getElementById('ss');
+
+let day_dot = document.querySelector('.day_dot');
+let hr_dot = document.querySelector('.hr_dot');
+let min_dot = document.querySelector('.min_dot');
+let sec_dot = document.querySelector('.sec_dot');
+
+let endDate = '01/01/2023 00:00:00';
+
+// date format mm//dd/yyy
+
+let x = setInterval(function(){
+    let now = new Date(endDate).getTime();
+    let countDown = new Date().getTime();
+    let distance = now - countDown;
+
+    // time calculation for days, hours, minutes and seconds
+
+    let d = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let s = Math.floor((distance % (1000 * 60)) / (1000));
+
+    // output the result in element with id
+
+    days.innerHTML = d + "<br><span>Kun<span>";
+    hours.innerHTML = h + "<br><span>Soat<span>";
+    minutes.innerHTML = m + "<br><span>Daqiqa<span>";
+    seconds.innerHTML = s + "<br><span>Soniya<span>";
+
+    // animte stroke
+
+    dd.style.strokeDashoffset = 440 - (440 * d) / 365;
+    hh.style.strokeDashoffset = 440 - (440 * h) / 24;
+    mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+    ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+
+    // animte circle dots
+    day_dot.style.transform = 'rotateZ(${d * 0.986}deg)';
+    hr_dot.style.transform = 'rotateZ(${h * 15}deg)';
+    min_dot.style.transform = 'rotateZ(${m * 6}deg)';
+    sec_dot.style.transform = 'rotateZ(${s * 6}deg)';
+
+    if(distance < 0){
+        clearInterval(x);
+        document.getElementById("time").style.display = 'none';
+        document.querySelector(".newYear").style.display = 'block';
+    }
+
+})
